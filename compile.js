6c0baee6f0e808412066dc23f5124350371a6e68
cocoknightcom <contact@cocoknight.com>
2023-11-27 21:33:55 +0100

    added toggle to switch code blocks on and off

--- compile.js
+++ compile.js
@@ -14,7 +14,7 @@ const formattedHTML = htmlContent
 
 const formatObject = {
     name: "CopyPaste",
-    version: "1.2.7",
+    version: "1.3.5",
     url: "https://github.com/cocoknightcom/CopyPaste",
     author: "cocoknightcom",
     description: "A Twine 2 Proofing Story Format that cuts it. Filter passages by tag.",
--- src/script.js
+++ src/script.js
@@ -15,6 +15,12 @@ document.addEventListener('DOMContentLoaded', function () {
     tagElements.forEach(function (tagElement) {
         availableTags.push(tagElement.getAttribute('name'));
     });
+    
+    // Create CopyPaste Icon
+    var iconImage = document.createElement('img');
+    iconImage.src = "https://cocoknight.com/CopyPaste/dist/icon.svg"
+    filterList.appendChild(iconImage);
+    filterList.appendChild(document.createElement('hr'));
 
     // Create "Activate All" button
     var activateAllButton = document.createElement('button');
@@ -66,6 +72,43 @@ document.addEventListener('DOMContentLoaded', function () {
         filterList.appendChild(document.createElement('br'));
     });
 
+    filterList.appendChild(document.createElement('hr'));
+
+    // Create a fun toggle button
+    var toggleButton = document.createElement('div');
+    toggleButton.style.width = '40px';
+    toggleButton.style.height = '20px';
+    toggleButton.style.borderRadius = '10px';
+    toggleButton.style.backgroundColor = '#ccc';
+    toggleButton.style.cursor = 'pointer';
+
+    // Create the switch handle
+    var switchHandle = document.createElement('div');
+    switchHandle.style.width = '18px';
+    switchHandle.style.height = '18px';
+    switchHandle.style.borderRadius = '50%';
+    switchHandle.style.backgroundColor = '#fff';
+    switchHandle.style.position = 'relative';
+    switchHandle.style.left = '2px';
+    switchHandle.style.transition = 'transform 0.3s ease-in-out';
+
+    // Append the handle to the button
+    toggleButton.appendChild(switchHandle);
+
+    toggleButton.addEventListener('click', function () {
+        toggleExtractStrings();
+        
+        var showCodeBlocks = toggleButton.dataset.showCodeBlocks === 'true';
+
+        // Update the dataset attribute for the next toggle
+        toggleButton.dataset.showCodeBlocks = (!showCodeBlocks).toString();
+
+        // Move the switch handle to the new position
+        switchHandle.style.transform = showCodeBlocks ? 'translateX(0)' : 'translateX(20px)';
+    });
+
+    filterList.appendChild(toggleButton);
+
     document.body.appendChild(filterList);
 
     // Function to toggle all checkboxes
@@ -78,6 +121,19 @@ document.addEventListener('DOMContentLoaded', function () {
         var selectedCheckboxes = Array.from(filterList.querySelectorAll('input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
         filterElements(selectedCheckboxes, activateUntaggedCheckbox.checked);
     }
+
+    // Function to toggle extracting strings in parentheses
+    function toggleExtractStrings() {
+        // Toggle the state
+        extractStrings = !extractStrings;
+
+        // Trigger the filter based on the updated state
+        var selectedCheckboxes = Array.from(filterList.querySelectorAll('input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
+        filterElements(selectedCheckboxes, activateUntaggedCheckbox.checked);
+    }
+
+    // Variable to store the state of extracting strings in parentheses
+    var extractStrings = true;
 
     // Function to filter elements based on selected tags and untagged option
     function filterElements(selectedTags, includeUntagged) {
@@ -95,8 +151,11 @@ document.addEventListener('DOMContentLoaded', function () {
                 // Get the 'name' attribute value
                 var passageName = passage.getAttribute('name');
 
-                // Extract strings in parentheses and remove them from the content
-                var passageContent = passage.textContent.replace(/\(.*?\)/g, '');
+                // Extract strings in parentheses and remove them from the content if enabled
+                var passageContent = passage.textContent;
+                if (extractStrings) {
+                    passageContent = passageContent.replace(/\(.*?\)/g, '');
+                }
 
                 // Trim leading and trailing whitespace and line breaks
                 passageContent = passageContent.trim();
@@ -126,7 +185,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var selectedCheckboxes = Array.from(filterList.querySelectorAll('input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
     filterElements(selectedCheckboxes, activateUntaggedCheckbox.checked);
 
-    // Add change event listener to the checkboxes and untagged option
+    // Add change event listener to the checkboxes, untagged option, and extract strings toggle
     filterList.addEventListener('change', function (event) {
         var selectedCheckboxes = Array.from(filterList.querySelectorAll('input[type="checkbox"]:checked')).map(checkbox => checkbox.value);
         filterElements(selectedCheckboxes, activateUntaggedCheckbox.checked);
